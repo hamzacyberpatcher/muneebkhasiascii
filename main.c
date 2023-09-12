b8ded599bd1134a190380c80491b183f808f7492
hamzacyberpatcher <87220012+hamzacyberpatcher@users.noreply.github.com>
2023-09-12 19:12:25 +0500

    Added the code file
    This file contains the code of the animation

---
+++ main.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(void)
+{
+	system("mode 150");
+	system("title Muneeb is a khasi bakra");
+	Loading_Bar();
+	muneeb_1();
+	muneeb_2();
+	typer();
+	dis_1();
+	dis_2();
+	getch();
+}
+Loading_Bar(void)
+{
+	int k=1;
+	printf("\n\n\n\n\n\n\n\n");
+	printf("\t\t\tLoading..................\n\n");
+	printf("\t\t\t");
+	while(k<=25)
+	{
+		printf("%c",177);
+		k++;
+	}
+	printf("\r\t\t\t");
+	k=1;
+	while(k<=25)
+	{
+		printf("%c",219);
+		sleep(0.9);
+		k++;
+	}
+}
+typer(void)
+{
+	system("cls");system("color 0a");
+	printf("\n  __  __                       _       _               _    _               _ ");
+	printf("\n |  \\/  |                     | |     (_)             | |  | |             (_)");
+	printf("\n | \\  / |_   _ _ __   ___  ___| |__    _ ___    __ _  | | _| |__   __ _ ___ _ ");
+	printf("\n | |\\/| | | | | '_ \\ / _ \\/ _ \\ '_ \\  | / __|  / _` | | |/ / '_ \\ / _` / __| |");
+	printf("\n | |  | | |_| | | | |  __/  __/ |_) | | \\__ \\ | (_| | |   <| | | | (_| \\__ \\ |");
+	printf("\n |_|  |_|\\__,_|_| |_|\\___|\\___|_.__/  |_|___/  \\__,_| |_|\\_\\_| |_|\\__,_|___/_|\n\n\n\n\n");
+	
+	
+	printf("\n  _           _              ");
+	printf("\n | |         | |             ");
+	printf("\n | |__   __ _| | ___ __ __ _ ");
+	printf("\n | '_ \\ / _` | |/ / '__/ _` |");
+	printf("\n | |_) | (_| |   <| | | (_| |");
+	printf("\n |_.__/ \\__,_|_|\\_\\_|  \\__,_|");
+	sleep(2);
+	//system("espeak \"Munneb is a khasi bakra\" -s 90");
+}
+
+dis_1(void)
+{
+	system("cls"); system("color 0f");
+	printf("\n               .^^~^:         :^~~:               ");
+	printf("\n             ^?JJJJYY?^     .?YYYJY?              ");
+	printf("\n            ~5??????J5P!!7!7?5Y?7??Y?:            ");
+	printf("\n           ~Y~. .?7Y?~^^!?7~^:J5?7  ~Y!           ");
+	printf("\n      :^^^~?^.:^:!5?~~~^:::^~~!JG7^^^~J7!~~!:     ");
+	printf("\n      !7!7?YY?5B5777777Y5YPY77777?5B555PJ!77:     ");
+	printf("\n       :!!!7?YGGGPGGPPPPY7YGPPPGGGPBG?77!~:       ");
+	printf("\n         .::::~5#&&&&&&G:.:G&&&&&&#5:..           ");
+	printf("\n               7G5PGBPJ^:::^JPBGP5G~              ");
+	printf("\n                J!::7~.......:J^:J!               ");
+	printf("\n                ^P~:~7~5J7?5?~7^??                ");
+	printf("\n                 5?:^?:?J7??~^?7Y                 ");
+	printf("\n                .7JJ^JJ?7?77?J7JJ~                ");
+	printf("\n               ~5~:7??J7!~!!7YJ~^P?               ");
+	printf("\n              ^~G7::^5~~!!!!^7?:^P?^              ");
+	printf("\n             ^^ ~G7::7J~::::~Y~:!G::~             ");
+	printf("\n            :^   ~5YJ77YY7??Y~^?P7  :~            ");
+	printf("\n           :^      ^~77?5G55J?J?:    :^           ");
+	printf("\n           JJ^           .. ..       :P7      .::^");
+	sleep(2);
+}
+
+dis_2()
+{
+	system("cls"); system("color 0a");
+	printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXK0Okkxk0XMMMMMMMMMMMMMMMMMMMMN0OkxdxOKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMW0ddl:;:c::;;:dKWMMMMMMMMMMMMMMMNx:::;:::::lKMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMW0c:loloooodoclc:;dXMMMMMMMMMMMMW0c;collloodo'cNMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMM0::xdoxkdooooodool;;o0XXNWMMMWNXk:;lolllooooxo;xNMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMW0c;clloooddoodooc;,.'cdddollddololcc;cloxxxxdll;'c0WMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMM0:,:oOKNXkldkolo:,:lox00Okc,:clodkK0Ol,;odxxll0XOo,'cOWMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMK;,o0WMMMMWkcdOx;'dKKXXXK0OOOxodk00KXX0l':xxloXMMMMXx,'kMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMXol0WMMMMMMMWxloccdKXXXXXXXKKKKKKXKKXXXXOl,,;cKMMMMMMMXdlKWWWWWWWWMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMWX0OOOOOOlcOXNWMMMMWMMNo;d0KKKXXXXXXXXXXXXXXXXXXXX0kl':0NWWWWN0kxookkxxkkkkxkNMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMNkdddxxdddddxxkkxocccccc,':ccclllodxk0XXXX0Oxdolcccc::'..;cccccdkdlccoxkO00OklkMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMXoo00Oxollc:coxkx:...,cooooooooolc;'';ccc:;'';cllooooooool,...cd:'',,':k00kdoxXMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMXkddO0Okkx:'.',::...lKK000KKXXXXXKl........lKXXXXXXK000KKo...''';::;cx0kodkXWMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMNOddxO000Okdc'.....'.........'';:,;oooo:';;''.........''....cxkxxxdddx0NMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMN0kxxxdddddl',;..            ..;kNNNW0,.            ....';oxxkkkOKNMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMWNXK00K0Oxl,..             .lXNNNNXl.             ..,kKXNWMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNc ..          ..:0NNNNNN0:..          ...lNMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNl..'..........,o0NNNNNNXN0o,..........,..kMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc;odoolc;,cdx0NNNXNNNNNXNNKxoc',cllodo,:XMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0;c0KXXX0odXNNNNNNNNNNNNXNNNXk:oKXXKx:lKMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNl;k0XXXXkcxNNNWWWNNNNNWNNXNNx:kXXKO;:XMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0;;kKXXXKodNKdodk0KK00ko:dXKloKXKOc;OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWl.c0XXXKodXO'.,',::;,,'.cXOcxK0x::0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMk.,kKXXXxl0Xx:.'okkd:.,d0NKoo0k,;0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXc,x0XXXxcONK0kxOOxkOk0KKXNklOO;cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd,:lkKXx:ddcoO0OOdx00Olcxkld0d;:OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWOldx::kX0c,clcclodccoollll::kk:ldldXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0ookKKk::xOx:l00kxoooodxOK0o:locd0Od,;KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk',xKXXXOollooclx0XXXXXXKOdllc;:kKX0x' lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0o..o0KXXXXKd;d0xoodddxxddook0k:oKXK0x'.lkXMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMMWOkXl.:k0KXXXKo'lKXK0kxdddxk0KXKk:lKXK0k,'OOkNMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMMWOkNMX:.ck0XXXXk;;kXXXXXXXXXXXXK0o;xKXK0x',KNkONMMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMW0kXMMM0;.ckKXXXKk:;x0XXXXXXXXXK0o;oKXXKOl.lWMNkkWMMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMMKxKMMMMM0,.;d0KKKKOl,cx0KXXXKKK0l;o0XXK0d''OMMMNkOWMMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMMNk0WMMMMMMKo'.coox0K0xc,:xOOxdxd:;dKXXKkc..xWMMMMXxOWMMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMMNkOWMMMMMMMMW0o;..',:coxo;;;,,;:';x000kl'.lKWMMMMMMXxOWMMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMMNkkNMMMMMMMMMMMMNKKOolc;,,..  ..'.',,,,''c0WMMMMMMMMMXxOWMMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMWOkNMMMMMMMMMMMMMMMMMMMMWKOkddddxxxxdxkO0XWMMMMMMMMMMMMXxOWMMMMMMMMMMMMMMMMMMMM");
+	printf("\nMMMMMMMMMMMMMMMMMMMMMXl:xKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO:'xWMMMMMMMMMMMMWWWNNNX");
+	sleep(2);
+}
+muneeb_1(void)
+{
+	system("cls"); system("color 0d");
+	printf("gmg@ggmHg$@$@gk!*|j@@@w@{kikk}|`_______________,,;g@@@@@@@@@@@@@@@@@@@@@@@@@@@@HbH@@p,,______`_,*~kjj{yjl}j|;,_._`::i|$$$$$$$$$");
+	printf("\nH@@N@Hk]]g$$$$@j{[]]@@N{!jjk|r_____________,;gg@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NHbkHMHkbHb@Hw__________![|j)k|j|!|____`!|jj$$$$$$$");
+	printf("\ngH]Nbg&@$$$&@NH|j|]@QK#|jjir`__________,yp@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NNNNk||*!**||||||~~!~.___________!j*!!||},____`!|]$$$$$$");
+	printf("\nHpg@@@@@@$$$@Mhljj]w$||jj{!`________.p@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NMHMjH|r~_!-_,,|,||||;:________,,,______}j||!|,______!|$$$$$");
+	printf("\nggjkkkp@j]R@Mg]ij{w|N!!jjr`_______,p@@@@@@@@@@@@@@@@@@@@@@@@@NHMH[|k!^,_`_``____;|jyy||lwr*^``_;n@|&&@U_____`!!|:!:,_____~]$$$$");
+	printf("\n@@ijjjj@|]@NH@Nl!!]j|jji!`______,@@@@@@@@@@@@@@@@@@@@@@@@@MNMM||{Y!_`_______,;wef*^,_,,,,,,___T***H|[_________`j!!!_``____`]$$$");
+	printf("\n@Nkjllj@@N$g]@|j!||M!il=`_____,ppH@@@@@@@@@@@@@@@@@@@@@NHhwk|||!~_____,~=^`,,~;p@@@pgj||||||C___;mjkg;,_________!!,:______``]$$");
+	printf("\n@@bggg@Hmmb]Nh_j!|{/jjr______]MkkkkMbKKHM@HM#KMh]M[jMj]kjjk|||~!_.~``_._``_;@@@NMNMMHbbHk|||]__]mmpmmgmb,_________`~.`~______]$");
+	printf("\n@HH]@@Mj@F||||;jk|{{k|~_____j|||!||!!*=!!**_``_`^|!||jppgggk;F^`_,~``____;#kF*^_____;||||||jg@_]@@@@HHmgk!__________!,_______`$");
+	printf("\n]@@]@MM{|,|kppkg@ljj|~_______*__``__________________|]m]@M*,___m||::____!||;p,,,,:.)kl;r!;]@@@.]@@@@@@Hpm!|___________`_______]");
+	printf("\nmhkjk!|j@]@ph||||klir__________,__,;,,,,,____,,=~~____::______]W|||;;;/|||]@HH@pgpgkkj||!jm@@@U_@@@@@@@@@l!!__________`________");
+	printf("\n|!|g@@@j|j]Hkk]gyjjF________,;yygjmN*!?^^`__,,.=,,_____,j;gwo__]_`jjHHjg@HUkUj{j}kl{|||;gp@@@@*]@@@@@@@@hkl|!,________________]");
+	printf("\nHj#H]Mkkg|j@kij]N@@~________||{k*``,,wm#M****`____]___j@@&&@@@,_]m_]gggmkgkkkjjjjjj|gp@@@@M|)g@@@@@@@@HHbki!l!,__________`____$");
+	printf("\n@kjjijm]M]kn!j{jk#k,______-^`_,=`]bM*:_______.;,__|@__@@&&&&@@@@__N/]@@@pH@@@@@@@@@@MMj]g@@@@@@@@@@HHbmkkkkili|____________`_$@");
+	printf("\nkgkjjj]@@@]gjm|jjkk________`_!|;:**,;yp;,_`,;pgp@@@H__@@&&&&@@@@@@_*R@@@@@@@@MM||gg@@@@@@@@@@@@@@@@HHbmgkkkjl||______________|!");
+	printf("\np]@mg]@kkj@Hpgk!yr`____a`____]gkk!;;|jkkgpHHM|||l]ppC~@@&&&&@@@@@@@@;,;ywgg@@@@@@@@@@@@@@@@@@@@@@@HHbbkkkkkki!!______,,;;;___!|");
+	printf("\nKHHHHm@@@]@@NHmp@!;^___@,,____k@@@@pjj{!jjjljjjk@@@@U]@@&&&&@@@@@@@@@@@@|jMN@@@@@@@@@@@@@@@@@@@@@@Hbbbkkkkjjll|___:;|]@@@@@,/lj");
+	printf("\nNN@@@@&@@Njp@MNN!:,____]@!h___]@@@@@@pkjjllgp@@@@@@M_@@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@mbkkkij#kl!|___!|j]@@@@M|!!!");
+	printf("\n@@@@NHjj@@$$&@@gp@kkjb__]@j#,_*@@@@@@@@@@@@@@@@@M|z@]@@&&$$&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@HHbgiljjjjiL____!|jMMh*,___.");
+	printf("\n&@@@Hk]@@@@Q@@@jMkkgkjp__]@NMU_]@@@@@@@@@@@M*]gg@@@|@@@&&&$$&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NHHbkkk}jl!jli|__________:|||||");
+	printf("\n@@kkkk#Q[pp$H@$@@p@hkkm@,_^R@gpp]@@@@M*|;gg@@@@@NMj@@@@&&&$$&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Hbkkkki||ji!!!!|__________.:||||");
+	printf("\nNNkkkk]]R@@@@@@@@@mkj#mmgw___||];ywm@@@@@@@@@NMkp@@@@@@@@@&@@@@@@@@@@@@@@@@NM]@@@@@@@@@@@H@bHbbkkikl!;||||il||,___________:||||");
+	printf("\n$@pmkk#@@@R@R@@Mj#kkkkjjj]H__]jj]@@@@@@@@NUkp@@@@@@@@@@@@@@@@@@@@NMjj#MMM!!||j]@@@@@@@@NNHkkkkkjkkjji!;!||l|||,___________:||||");
+	printf("\nQ@pkkk]@@@@@@$@@@kj{jkijm@M@__mm]HH@@@@kp@@@@@@@@@@@@NN@@@@@@@NHk||*^___,;|||jj]@@@@Hkk||||jjjkgkkgklll||||||!___________.:||||");
+	printf("\n@@k#kk]@@@@@&&@Nbgijjijmkkk]$g]H@@HHb@@@@@@@@@@@@@@@MM*jjjj#Mj|||!____:!!,||||j]HHbHHmbk*||||j#kkmkkki|||j|||___________.:||!jj");
+	printf("\n@Nkkkk]N@N@@@@NNMhkgl#mmgkj]$$W]]@@@@@@@@@@@@@@@@@@@@@r___^!!|||!________!|||gHHHHbHki||!!!!|jmkmmmkj|||||||.___________:!|||jj");
+	printf("\ngkkkkj]HHbbbkkmpkjj]Ubmg]@@@RRR@gHH@@@@@@@@@@@@@@@@@@@@@y,_______________;y]gmmH@mbbki^___,;jgmmkkkk|||||||:___________::!||llj");
+	printf("\nNbUkkkmmH#mkkH@Hbgkkkkk]N*|j$@$$@kkk####HbbHH@@@@@@@@@@@@@m;,,________,p@@@@N@NM**!~_,,;;y]@pmmkkkk||||||:.____________:::||llj");
+	printf("\nbkkkkkkkmkkkkkmmmmmkmgmkg]@@#R@@R@j{jjkjkkkkkk}{!kk]HHHmmbp]@@@@g;ggg@@NMQk}k*!:,;;ggpmkj]@Hbmmkkk|||||!:.____________.::|||ll!");
+	printf("\nkkkkkkkUmmmgkkkmbmmkkjkkjj]@@mHHH@k|!!jjjijjk|||*!!|jmH@@@@NN@MNBNNNNhhM**!:;w@@Hbkkmk*jg@@HbHkkk|||::::______________.::|!|!lj");
+	printf("\n|j#kkjijjHbmmkkkkjgmmmkkmHH*L|k@||*N@!|jjjjkkkj|L_``**M*|jkKMMM*j*|{!|:;gg@@@NHjmHHM||]]HHHmbkkk!||::::_______________,:||lljjj");
+	printf("\n|jijjjkmkkk#kmkmbmmkkmmhkbb|g||]k@pg@@|||jjkkkkli|ypppp;;;yppgggwwwgg@@@@@@NNk@HM||||]]HbHmbkkki|||:::________________:,|jiiWkj");
+	printf("\n#MMk{jjkkkgig@gg@@pg@@bggkjkmkHmhp@Nkjjg|!jjjkkkljjmHHHHH@H@@@@@@@@@@@@NNNMkh*!:,,|j]mHmbkkkkki!||:.__`______________::|jljjjkk");
+	printf("\nk!ggggkkjjj]@@@B&&&$$$@pg;jjj@RHbk][]N@&$W|jj!jjijj#HHH@mjMKNNNUMMMMMMj|!:_____.;;@@@HHbmmkkkki||~__________________.,|jljjjjkk");
+	printf("\njjjkjmmkkmmmkmR@$$$$@@@$@pgkmbkmm#HHjj$@Q@@N|||||jjj#HHHHHH@g;,.___________,,yg@@@@@@@@HHmkkki|L___________________,,|!jjjjjkkk");
+	printf("\njmmmmmmgkmHmpg$$$$N$$$$&$&@|jMjj|l!jj]H@N@@$@@||||||jj#HmmHHbppggy;;pw@@@@@@@@@@@@@@@@HHbmkk||!____,!_____________,;jjjjjkjjjkk");
+	printf("\nj#kkkkk$$@@&@&@Q@$$$@&$$&$@&@kr;|g||]pji]@jR@@g@@g;||||jjkkmHHH@@@@@@@@@@@@@@@@@@@@HbHbbk{k||`___,!,`____________;jjijjjjjjkkkk");
+	sleep(2);
+}
+
+muneeb_2(void)
+{	
+	system("cls"); system("color 0b");
+	printf("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@!_________________________||;!!jjkkkkkkkkkkkkkkkkgpmg!||||:,___;|!!!|j!i!j;jB$$$$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&[~__________;;,,;,,,,,,;;;;!!!jjjkkkkkkkmmmmmmmmmmHHHmmpp;||||||||||!|||;;{!j$@&&$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&$H__________|!!j;;;;!;;;;;j;jjjjkkkmmmmHHHHHHHHHHHHHHHHHHHH@L,__||::.,!!_|;!!!jR&$$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&$$&&&&&&&&@r_________|!;!!!!!!!!!!jj;gmmpmppUUHHHHHHHHHHHHHHHHmHHHHHHHH@w,_::__,,,;|!|||jB&$$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&$$&$$$$&&&&&&&$[~_________|;!!!!!!jjgmmpUHH@@@@@@@@@@@@@@@@@@@HHHHHHHHHHHHHHHH@m!,___!|||||||j]&$&$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&$$$$$$$&&$$&&&@h_________,jjjjjkkmmkkUUUHHHHHHH@@@@@@@@@@@@@@@@@@@HHHHHHmHHHmmmbk!___|::::|||;]&&$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&$$$&&&&&&&&&&&N!________,jjjjkkr||||*|jjjkkkUHHHHHHHHH@@@@@@@@@@@@HHHHHHHHHmmmmhk!___|:,:||||!$&&$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&$&&&&&&&&&&&@H:_______,jjj||!::._______!!!!jjjjjUUUUHHHHHHHHHHHHHHHHHHHHHHmmmmmkL___::|_|||||$&&&&$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@_______!*khk!!:::,,,,,,_________:j!j!jj{{{*jj{jj]hUUUHHHHHHHmmmmmk!_____:_|,|!!]&$$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@___,r;kN___;r;;;r;;;,,,,,`__._____::|||||!|::!:|!|!****!|j]UHHmmmm!________||||]$$$$$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@R@_]M*^!;,__]!|||!!;!!jjkkr|_``_._`___|;|;|,:______________|||j]Hmhkk.________||!]&&&&$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&@MMNh|L^___m)kh_rk;!|;!^,_______`_____:______________````_^_~,,_|||!jjUmk_________|||]B&&&$$$$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&N!|!j**___jjkkh_kkk!!!;:!!,_________,,;__;ppp,__;!!!!_____,,,;~,,,`***{mkr________,|j]&&&&&&&$$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&@@r|;!j!!___]jjkm_]kmki!!!|||!;;;|,_;;|^^,@@@@@@U_!|:_____!***j!!;!|!km,__^^,______:||]@&&&&&&$&$$&&$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&@U!jjjkr.:_jkjkmm@;*MHky!|||||||||||!!yr]@@@@@@H@_!|!!__,;______|jkr!!!j[__^#@._____||]&&&&&&&&&$&$$$&");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&@p!jkkk||__jjkmmHHH@@@@w;;|*****M**M**;#@@@@@@@@HU_k|||!||,,__,{mr,j;;;jr_/mjB@_____:pH&&&$&&&$&&&&&&&");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&@Wrmk{!,___]mmmHHHH@HUHHHHmmkkkkkkkkjj]@@@@@@@@@H@,^H;|;||||||||;mkkkjmm_@mpr_*____;p@@$&&&&&&&&&&&&&&");
+	printf("\n&&&&&@@@&@&&&&&&&&&&&&&&&&@gkk!|;;_;mkkmmHHHHHHHbHbbkjjj!jjjkk]@@@@@@@@@@@HHkHpw;***=;|;!jjjj]kpL]H]H[_____;]R&&&&&&&&&&&&&&&&&");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&@&Hmmkkkk_jkkkkkkHHHHHHHHHHHhkkkkjp@@@@@@@@@@@@@@@Hk;jjkkkHHpppwgg[[|]g@HHHH[_|;;p@|j$&&&&&&&$&&&&&&&&");
+	printf("\n&&@@&&@&&&&&&&&&&&&&&&&&&&@@mHpgkk|]kkkkjjjkkkkkkkkkkkjjkk]HHhhHUUUUHHHHHH@@@kkkjjjkkmmmHHHHHHHHbHHHHh_;j]@Hhj$&&&&&&&$$&&&&&&&");
+	printf("\n&&&&@&@&&&&&&&&&&&&&&&&&&&@@m!]HHp|jkk{{jjjjjjj{{!*!jjjjkk!|,^*jj!jjj*jjjj]KHHpjjkkmmmkkHHHHHHHHHHHHHh_;mk][!j$&&&&&&&&&$$$&&$&");
+	printf("\n&&&&&@@@&&&&&&&&&&&&&&&&&&&@H@Hhmh|j{!!!!!!!!!|!!!!|;jjjkkk!|!:_:||||!**^^**jjkkkjkkkkkmmmmHHHHHHHHHHh|j]HH[;j$&&&&&&&&&&&&&&&&");
+	printf("\n&&&&&&&&@&&&&&&&&&&&&&&&&&&@H@HHhk|j!!!!!!!!!|!___;!l!jjjkj!!|:___.!^::|:;;;jjjkk;{jj;jjkkkmmmmHHHHHHh|||j]]p]@&&&&&&&&$&&&&&&&");
+	printf("\n&&&&&@@&&&&&&&&&&&&&&&&&&&&&mMhk|;:j!!!!!!!;!!,;!^,,||;jj!!!;;;__________|!!jjjkkk!!!!!jj;jjkkkkmmmHb!|||!j]gg&&&&&&&&&&&&&&&&&");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@H|!!!!!!!jj!!;;;jmp,;|||;!!!||||||:!!*!j;j!!j!|!|,,_!!!!!jj;jjkmh!;g;]@Hg&&&&&&&&&&$&$$$&$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@W|!!!!!!!j!!!jjjjjkkjkkkUHH@g|;;;|;,,||||||***___|!;|;!jjjjjjkk!jk]hj]#$&&&&&&&&&$&&&&$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@Mj!:|||!|!||!jjjjkjkijjjj{{{;jjj{{{jkj{jUmmg;,,____;!;j!jjjijjjkj]p@@]@@&&&&&&&&&$&&&&&$$$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@M|||::,|||||||||!jj;jj;;ki!!|!!!|!!!!!jjjj;kkkkki!yr!!!!jjjj;kjjkk!!j]H@@@&&&&&&&&&&$&&&&$&&$$$$");
+	printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&@M!!!|!_||_.|||||||!!!jj;jjjj!;||,________,|||;!jjjjj{!!|!!jjjjjj;k;]@@pg]]g@&&&&&&&&$$$&&&$$$$$$$$");
+	printf("\n&@&&&&&&&&&&&&&&&&&&&&@@MMjkjj||||:,!||___:|||||||j!jjjjjjjkmmmpmp;;;,,;|;!jjjjjj!!!!!!!jjjjjrj@@@&&&&&&&&&&&&&&&$&$&&&$$$$$$$$");
+	printf("\n@@RRRRMMRMMMMMMMMMRMMM}jkkji!||||:,@U!||,___::||||!jjjjjjjkjjjj;jkk;;jkk!!!!!!!j!j!!!!!!!jjjjg@&&&&&&&&&&&&&&&&&&$&&&&&&&&&$$$$");
+	printf("\n@U@@@HHH]jhUjjjj]HHhkkkkkj{!!|||:_]@@!||,_____-___||!!jjjj!!j|j!!*!jjjjjjjjjjj{!!!!!!!!!jjjg@@&&&&&&&&&&&&&&&&&&&&$&&&&$$$$$&&&");
+	printf("\nHHHHHhjkk{|||jmk{kkkkjkk;j!!!!|,_j@@@@|||,__________:!||!|!|!!!|!|||||jjjj!!!!!!!!!!!jjjjj@@&&&&&&&&&&&&&&&&&&&&&&&&&&&$&&$&&&&");
+	printf("\nUkjk!{*!|||;ph}jkkkkkkk;jj!!!||:_#@@@@@||||,_____________|!|!|!!!|!|||.:|||!!!|!!!!!!!!!;]@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&$&$$&&&&");
+	printf("\nj;{!!!||||]hrjj)kkkkjkkk{!!!|||,]@@@@@@@;||||,_______________|!|!|!!!|!|||_:!!!||||||;jmmm]&@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&$$&&&&");
+	printf("\n!!!!|||;jk{!jmkkkkkkj;jij!!!|||,$@@@@@@@@@||||,,,___________________________,;;!;jjjkkk[j]&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&$&&&&");
+	sleep(2);
+}
